92844e0fe49deca7e3216090f47fc8a756db3856
Borza Viktor <viktor.borza@jaydata.org>
2017-06-06 13:44:46 +0200

    ADD: runtime provider plugins

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './classes'
+export { Service, FunctionalApi, FunctionalService, DynamoDB, callExtension } from './classes'
+export { addProvider, removeProvider, Provider, AWSProvider, LocalProvider, DeployProvider } from './providers'
 import * as _annotations from './annotations'
 export const annotations = _annotations
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,12 +2,32 @@ import { get } from 'lodash'
 import { constants, getOwnMetadata } from '../annotations'
 import { callExtension } from '../classes'
 
+export { Provider } from './core/provider'
+export { AWSProvider } from './aws'
+export { LocalProvider } from './local'
+export { DeployProvider } from './deploy'
+
+import { Provider } from './core/provider'
 import { provider as aws } from './aws'
 import { provider as local } from './local'
 import { provider as deploy } from './deploy'
 
-let environments = { aws, local, deploy }
-let invokeEnvironments = { aws, local }
+let environments = {}
+let invokeEnvironments = {}
+
+export const addProvider = (name, provider: Provider) => {
+    environments[name] = provider
+    invokeEnvironments[name] = provider
+}
+
+export const removeProvider = (name) => {
+    delete environments[name]
+    delete invokeEnvironments[name]
+}
+
+addProvider('aws', aws)
+addProvider('local', local)
+addProvider('deploy', deploy)
 
 export const getInvoker = (serviceType, params) => {
     const environment = process.env.FUNCTIONAL_ENVIRONMENT;
