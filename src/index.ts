9b636652c95ae29f970348219cf9b1eee510e21b
Borza Viktor <viktor.borza@jaydata.org>
2017-07-28 14:46:04 +0200

    CHANGE annotation export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 export { Service, FunctionalApi, FunctionalService, DynamoDB, SimpleNotificationService, S3Storage, ApiGateway, callExtension, PreHook, PostHook } from './classes'
 export { addProvider, removeProvider, Provider, AWSProvider, LocalProvider } from './providers'
-import * as _annotations from './annotations'
-export const annotations = _annotations
 
-
-export { IHttpMethod } from './annotations'
+export {
+    templates, applyTemplates,
+    injectable,
+    apiGateway,
+    httpTrigger,
+    rest, httpGet, httpPost, httpPut, httpPatch, httpDelete, IHttpMethod,
+    environment,
+    tag,
+    log,
+    functionName, getFunctionName,
+    dynamoTable, __dynamoDBDefaults,
+    sns,
+    s3Storage,
+    eventSource,
+    classConfig, getClassConfigValue,
+    simpleClassAnnotation,
+    expandableDecorator,
+    use,
+    description,
+    role,
+    aws,
+    azure,
+    param, serviceParams, createParameterDecorator, request, context, error,
+    inject,
+    constants,
+    defineMetadata, getMetadata, getMetadataKeys, getOwnMetadata, getOverridableMetadata
+} from './annotations'
